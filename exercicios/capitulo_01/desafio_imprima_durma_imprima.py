4fcd9e88e6d81b2b5d56a21f4af04964aec9d7b5
Rebeca <rebecadivinapc@gmail.com>
2025-03-07 12:43:26 -0300

    desafio cap1

---
+++ exercicios/capitulo_01/desafio_imprima_durma_imprima.py
@@ -0,0 +1,21 @@
+
+    # Para rodar: 
+    # 1 - python -m venv venv
+    # 2 - .\venv\Scripts\activate.bat
+    # 3 - va ate o diretorio
+    # 4 - python desafio_imprima_durma_imprima.py
+    #para sair: deactivate
+
+# 1. Importe o módulo time,
+import time
+# 1. Imprima na tela Olá, mundo!,
+print("Olá, mundo")
+# 1. Insira a função time.sleepe passe como argumento 2,
+time.sleep(2)
+# 1. Imprima na tela Agora sou uma pessoa programadora Python. e Aguarde 4.5 segundos,
+print("Agora sou uma pessoa programadora Python")
+# 1. Insira a função time.sleep e passe como argumento 4.5,
+time.sleep(4.5)
+# 1. Imprima na tela Última linha do programa.
+print("Última linha do programa")
+ 
