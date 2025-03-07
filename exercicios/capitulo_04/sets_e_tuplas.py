f6686238b7bf00ed61908614dcb49b10bebc114b
Rebeca <rebecadivinapc@gmail.com>
2025-03-07 16:07:18 -0300

    cap 4

---
+++ exercicios/capitulo_04/sets_e_tuplas.py
@@ -0,0 +1,14 @@
+# - Crie um conjunto\n"
+numeros = {1,2,3}
+# - Converta uma lista em um conjunto\n"
+lista = [1,2,3,3,3]
+conjunto = set(lista)
+lista - list(conjunto)
+print(lista)
+# - Insira novos valores no conjunto\n"
+# - Remova um elemento do conjunto\n"
+# - Verifique se um elemento está contino no conjunto"
+# - Crie uma tupla\n"
+# - Imprima na tela um elemento da tupla\n"
+# - Crie uma função que retorne mais de um elemento\n"
+# - Atribua cada elemento da função em uma variável"
