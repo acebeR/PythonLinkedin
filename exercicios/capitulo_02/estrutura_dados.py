35c65f397454da2e9be595094c60bb12c3257a27
Rebeca <rebecadivinapc@gmail.com>
2025-03-07 13:03:17 -0300

    tentando cap02

---
+++ exercicios/capitulo_02/estrutura_dados.py
@@ -0,0 +1,20 @@
+# - Crie uma lista vazia
+lista = []
+# - Crie uma lista com um único tipo de dado
+listaInteiro =[1,2,3]
+# - Crie uma lista com diferentes tipos de dados
+listaConfusa = [1,0.1,"Rebeca"]
+# - Descubra o comprimento das listas criadas
+print(len(lista))
+print(len(listaInteiro))
+print(len(listaConfusa))
+# - Crie uma tupla - nunca pode adicionar ou modifica-la
+tupla = {1,'1'}
+# - Crie um conjunto
+conjunto = {1,50,100}
+# - Imprima o conjunto na tela
+print(conjunto)
+# - Crie um dicionário
+objeto = {'nome':'Rebeca','idade':26}
+# - Imprima na tela o valor correspondente a uma das chaves do dicionário
+print(objeto)
---
+++ exercicios/capitulo_02/variaveis_tipos_dados.py
@@ -0,0 +1,15 @@
+# - Declare uma variável do tipo integer
+inteiro = 8
+# - Declare uma variável do tipo float
+decimal = 0.1
+# - Declare uma variável do tipo complex
+complexo = 5+2
+# - Declare uma variável do tipo string com aspas simples
+texto = 'Rebeca'
+# - Declare uma variável do tipo string com aspas dupla
+texto1 = "Rebeca Carvalhedo"
+# - Declare uma variável do tipo string com aspas tripla
+textp2 = '''Rebeca Carvalhedo'''
+# - Declare variáveis do tipo booleano
+verdadeiro = True
+falso = False
