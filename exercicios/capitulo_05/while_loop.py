e2322b4c3d15d38044cd0aae5a43169e52e7bc6d
Rebeca <rebecadivinapc@gmail.com>
2025-03-12 07:45:59 -0300

    inicio cap05

---
+++ exercicios/capitulo_05/while_loop.py
@@ -0,0 +1,12 @@
+# - Crie um while loop que imprima números de 0 a 5\n"
+i = 0
+while(i <= 5):
+    print(i)
+    i += 1
+# - Crie um while loop com else"
+j = 0
+while(j == 5):
+    print(i)
+    i += 1
+else:
+    print("Não é o caso")
