8f713f27b254b148565377a81823d62b62b7db59
Rebeca <rebecadivinapc@gmail.com>
2025-03-12 10:12:49 -0300

    cap07

--- exercicios/capitulo_07/customizando_excecoes.py
+++ exercicios/capitulo_07/customizando_excecoes.py
@@ -1,3 +1,25 @@
-# - Imagine que você está criando um servidor web e customize exceções para o código\n"
-# - Simule a aplicação de uma exceção customizada no código"
- 
+# Definindo uma exceção personalizada para o servidor
+class ServidorWebErro(Exception):
+    def __init__(self, mensagem, codigo_erro):
+        super().__init__(mensagem)
+        self.codigo_erro = codigo_erro
+
+# Função simulando um servidor web simples
+def processar_requisicao(url):
+    try:
+        if url == "/erro":
+            raise ServidorWebErro("Erro interno do servidor!", 500)
+        elif url == "/pagina_nao_encontrada":
+            raise ServidorWebErro("Página não encontrada!", 404)
+        elif url == "/sucesso":
+            return "Página carregada com sucesso!"
+        else:
+            raise ServidorWebErro("URL desconhecida!", 400)
+    except ServidorWebErro as e:
+        return f"Erro {e.codigo_erro}: {e}"
+
+# Testando as requisições
+print(processar_requisicao("/sucesso")) 
+print(processar_requisicao("/erro"))  
+print(processar_requisicao("/pagina_nao_encontrada")) 
+print(processar_requisicao("/outro_endereco")) 
